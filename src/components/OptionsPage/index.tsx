a16bd351c113e0d61f19867f1cdaea69e3ad5c78
taqm <warabii.taqm@gmail.com>
2020-10-11 01:31:14 +0900

    :sparkles: オプションページのコンポーネント用意

---
+++ src/components/OptionsPage/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+const OptionsPage: React.FC = () => {
+  return <div>Hello World</div>;
+};
+
+OptionsPage.displayName = 'OptionsPage';
+export default OptionsPage;
--- src/options.tsx
+++ src/options.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-const App: React.FC = () => <h1>Hello Option Page</h1>;
-ReactDOM.render(<App />, document.getElementById('app'));
+import OptionsPage from './components/OptionsPage';
+
+ReactDOM.render(<OptionsPage />, document.getElementById('app'));
