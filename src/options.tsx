800269d9a19f7c6cb2486480c14139474db730af
taqm <warabii.taqm@gmail.com>
2020-10-15 21:41:16 +0900

    :sparkles: ルーティングの実装

--- src/options.tsx
+++ src/options.tsx
@@ -1,13 +1,22 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Route, HashRouter as Router, Switch } from 'react-router-dom';
 
 import ListPage from './components/ListPage';
+
+const Options = () => (
+  <Router>
+    <Switch>
+      <Route component={ListPage} path="/" exact />
+    </Switch>
+  </Router>
+);
 
 ReactDOM.render(
   <>
     <CssBaseline />
-    <ListPage />
+    <Options />
   </>,
   document.getElementById('app'),
 );
