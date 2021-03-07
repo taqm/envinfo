bb6a7e56593e0e1a1fb5bccc2b5ebb25046f7fab
taqm <warabii.taqm@gmail.com>
2021-03-07 17:17:23 +0000

    :sparkles: React&TypeScriptの導入

---
+++ src/options.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App: React.VFC = () => (
+  <h1>Hello World</h1>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
