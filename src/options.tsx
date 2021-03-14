455fda5a0163f3fda79c603b20d0ceea62240146
taqm <warabii.taqm@gmail.com>
2021-03-15 00:57:24 +0900

    :sparkles: ルーティングの追加

--- src/options.tsx
+++ src/options.tsx
@@ -1,12 +1,17 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { MemoryRouter, Route, Switch } from 'react-router-dom';
 
 import ListPage from './pages/ListPage';
 
 const App: React.VFC = () => (
   <ChakraProvider>
-    <ListPage />
+    <MemoryRouter>
+      <Switch>
+        <Route path="/" exact component={() => <ListPage />} />
+      </Switch>
+    </MemoryRouter>
   </ChakraProvider>
 );
 ReactDOM.render(<App />, document.getElementById('root'));
